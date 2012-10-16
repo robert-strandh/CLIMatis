740759424416e4fe773433d78cc09141d27dfe50
Robert Strandh <strandh@nocl.(none)>
2012-10-16 09:20:06 +0200

    added more detailed comment

--- Zone/genealogy-change.lisp
+++ Zone/genealogy-change.lisp
@@ -7,7 +7,9 @@
 ;;; This function is called when the children of some compound zone
 ;;; have changed, such as when a child has been added or removed.  
 ;;; 
-;;; FIXME: say more.
+;;; The default method specialized on ZONE signals an error.  forcing
+;;; the parent zone type to choose an action as a result of a call to
+;;; this function.
 
 (defgeneric notify-children-changed (zone))
 
