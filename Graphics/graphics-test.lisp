cf89cb66ba8d37e1ae663752a324fd7a9a7bb995
Robert Strandh <strandh@nocl.(none)>
2012-10-17 17:24:14 +0200

    more tests

--- Graphics/graphics-test.lisp
+++ Graphics/graphics-test.lisp
@@ -25,13 +25,32 @@
 ;;;
 ;;; Create a backend for testing.
 
-(defclass test-port (clim3-port:port) ())
+(defclass test-port (clim3-port:port)
+  ((%root :initform nil :accessor root)))
 
 (defmethod clim3-port:connect (zone (port test-port))
-  (declare (ignore zone)))
+  (setf (root port) zone)
+  (setf (clim3-zone:parent zone) port))
+
+(defun update (port)
+  (clim3-zone:ensure-gives-valid (root port))
+  (multiple-value-bind (width height)
+      (clim3-zone:natural-size (root port))
+    ;; This port imposes a size slightly bigger than the natural one.
+    (clim3-zone:impose-size (root port) (+ width 2) (+ height 4))))
 
 (defmethod clim3-zone:notify-child-gives-changed (zone (port test-port))
   nil)
 
 (defmethod clim3-zone:notify-child-gives-invalid (zone (port test-port))
   nil)
+
+(defun test2 ()
+  (let ((port (make-instance 'test-port))
+	(zone (clim3-graphics:opaque (clim3-color:make-color 0.5 0.5 0.5))))
+    (clim3-port:connect zone port)
+    (assert (eq (clim3-zone:parent zone) port))
+    (assert (eq (clim3-zone:find-client zone) port))
+    (update port)
+    (assert (= (clim3-zone:width zone) 2))
+    (assert (= (clim3-zone:height zone) 4))))
