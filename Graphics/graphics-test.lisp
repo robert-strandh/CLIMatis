08b7f4d1e0c3982d120605f3f572fd82c2ae1ee1
Robert Strandh <strandh@nocl.(none)>
2012-10-17 17:47:41 +0200

    more tests

--- Graphics/graphics-test.lisp
+++ Graphics/graphics-test.lisp
@@ -1,26 +1,5 @@
 (in-package #:clim3-graphics-test)
 
-(defun test ()
-  (let ((z (clim3-graphics:opaque (clim3-color:make-color 0.5 0.5 0.5))))
-    (assert (clim3-zone:gives-valid-p z))
-    (multiple-value-bind (result condition)
-	(ignore-errors (clim3-zone:invalidate-gives z))
-      (assert (and (null result) (typep condition 'error)))))
-  (let ((z (clim3-graphics:masked (clim3-color:make-color 1.0 0.5 0.5)
-				  #2A((0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0)
-				      (0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0)
-				      (0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0)
-				      (0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0)
-				      (0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0)))))
-    (assert (clim3-zone:gives-valid-p z))
-    (multiple-value-bind (w h)
-	(clim3-zone:natural-size z)
-      (assert (= w 11))
-      (assert (= h 5)))
-    (multiple-value-bind (result condition)
-	(ignore-errors (clim3-zone:invalidate-gives z))
-      (assert (and (null result) (typep condition 'error))))))
-    
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Create a backend for testing.
@@ -45,6 +24,27 @@
 (defmethod clim3-zone:notify-child-gives-invalid (zone (port test-port))
   nil)
 
+(defun test1 ()
+  (let ((z (clim3-graphics:opaque (clim3-color:make-color 0.5 0.5 0.5))))
+    (assert (clim3-zone:gives-valid-p z))
+    (multiple-value-bind (result condition)
+	(ignore-errors (clim3-zone:invalidate-gives z))
+      (assert (and (null result) (typep condition 'error)))))
+  (let ((z (clim3-graphics:masked (clim3-color:make-color 1.0 0.5 0.5)
+				  #2A((0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0)
+				      (0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0)
+				      (0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0)
+				      (0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0)
+				      (0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0)))))
+    (assert (clim3-zone:gives-valid-p z))
+    (multiple-value-bind (w h)
+	(clim3-zone:natural-size z)
+      (assert (= w 11))
+      (assert (= h 5)))
+    (multiple-value-bind (result condition)
+	(ignore-errors (clim3-zone:invalidate-gives z))
+      (assert (and (null result) (typep condition 'error))))))
+
 (defun test2 ()
   (let ((port (make-instance 'test-port))
 	(zone (clim3-graphics:opaque (clim3-color:make-color 0.5 0.5 0.5))))
@@ -67,3 +67,8 @@
     (assert (= (clim3-zone:width zone) 32))
     (assert (= (clim3-zone:height zone) 54))))
 
+
+(defun test ()
+  (test1)
+  (test3)
+  (test2))
