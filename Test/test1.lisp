5d0691f51a2a409145ddd0b804249c4cb6982c91
Robert Strandh <strandh@nocl.(none)>
2012-10-17 10:31:10 +0200

    add test for masked zone

--- Test/test1.lisp
+++ Test/test1.lisp
@@ -14,6 +14,18 @@
     100 200
     (clim3-graphics:opaque (clim3-color:make-color 1.0 0.2 0.1)))))
 
+(defparameter *za3*
+  (let ((a (make-array '(100 100))))
+    (let ((h (array-dimension a 0))
+	  (w (array-dimension a 1)))
+      (loop for r from 0 below h
+	    do (loop for c from 0 below w
+		     do (setf (aref a r c)
+			      (float (* (/ r (1- h)) (/ c (1- w))))))))
+    (clim3-graphics:masked (clim3-color:make-color 1.0 0.5 0.5) a)))
+	    
+
+
 (defparameter *zz2*
   (let ((text-color (clim3-color:make-color 0.3 0.1 0.0))
 	(text "this is a nonsense text to use as a test"))
@@ -23,7 +35,7 @@
       (clim3-layout:hbox
        (loop for word in (split-sequence:split-sequence #\Space text)
 	     collect (clim3-text:text word nil text-color)
-	     collect (clim3-layout:hbrick* 5)))
+	     collect (clim3-layout:hbrick* 7)))
       (clim3-layout:sponge*))
      (clim3-layout:vbrick* 20))))
 
