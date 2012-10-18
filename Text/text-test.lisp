a6afd9f14cfcc50bbb0ddd9fb69f7b1e59f1647c
Robert Strandh <strandh@nocl.(none)>
2012-10-18 04:49:33 +0200

    more tests

--- Text/text-test.lisp
+++ Text/text-test.lisp
@@ -1,65 +1,121 @@
 (in-package #:clim3-text-test)
 
+(defgeneric update (port))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
-;;; Create a backend for testing.
+;;; Port of type 1
 
-(defclass test-port (clim3-port:port)
+(defclass test-port-1 (clim3-port:port)
   ((%root :initform nil :accessor root)))
 
-(defmethod clim3-port:connect (zone (port test-port))
+(defmethod clim3-port:connect (zone (port test-port-1))
   (setf (root port) zone)
   (setf (clim3-zone:parent zone) port))
 
-(defun update (port)
+(defmethod clim3-port:disconnect (zone (port test-port-1))
+  (setf (root port) nil)
+  (setf (clim3-zone:parent zone) nil))
+
+(defmethod update ((port test-port-1))
   (clim3-zone:ensure-gives-valid (root port))
   (multiple-value-bind (width height)
       (clim3-zone:natural-size (root port))
     ;; This port imposes a size slightly bigger than the natural one.
     (clim3-zone:impose-size (root port) (+ width 2) (+ height 4))))
 
-(defmethod clim3-zone:notify-child-gives-changed (zone (port test-port))
+(defmethod clim3-zone:notify-child-gives-changed (zone (port test-port-1))
   nil)
 
-(defmethod clim3-zone:notify-child-gives-invalid (zone (port test-port))
+(defmethod clim3-zone:notify-child-gives-invalid (zone (port test-port-1))
   nil)
 
-(defmethod clim3-port:text-style-ascent ((port test-port) text-style)
+(defmethod clim3-port:text-style-ascent ((port test-port-1) text-style)
   (declare (ignore text-style))
   25)
 
-(defmethod clim3-port:text-style-descent ((port test-port) text-style)
+(defmethod clim3-port:text-style-descent ((port test-port-1) text-style)
   (declare (ignore text-style))
   15)
 
-(defmethod clim3-port:text-ascent ((port test-port) text-style string)
+(defmethod clim3-port:text-ascent ((port test-port-1) text-style string)
   (declare (ignore text-style string))
   20)
 
-(defmethod clim3-port:text-descent ((port test-port) text-style string)
+(defmethod clim3-port:text-descent ((port test-port-1) text-style string)
   (declare (ignore text-style string))
   10)
 
-(defmethod clim3-port:text-width ((port test-port) text-style string)
+(defmethod clim3-port:text-width ((port test-port-1) text-style string)
   (declare (ignore text-style))
   (* 8 (length string)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Port of type 2
+
+(defclass test-port-2 (clim3-port:port)
+  ((%root :initform nil :accessor root)))
+
+(defmethod clim3-port:connect (zone (port test-port-2))
+  (setf (root port) zone)
+  (setf (clim3-zone:parent zone) port))
+
+(defmethod clim3-port:disconnect (zone (port test-port-2))
+  (setf (root port) nil)
+  (setf (clim3-zone:parent zone) nil))
+
+(defmethod update ((port test-port-2))
+  (clim3-zone:ensure-gives-valid (root port))
+  (multiple-value-bind (width height)
+      (clim3-zone:natural-size (root port))
+    ;; This port imposes a size slightly bigger than the natural one.
+    (clim3-zone:impose-size (root port) (+ width 3) (+ height 5))))
+
+(defmethod clim3-zone:notify-child-gives-changed (zone (port test-port-2))
+  nil)
+
+(defmethod clim3-zone:notify-child-gives-invalid (zone (port test-port-2))
+  nil)
+
+(defmethod clim3-port:text-style-ascent ((port test-port-2) text-style)
+  (declare (ignore text-style))
+  21)
+
+(defmethod clim3-port:text-style-descent ((port test-port-2) text-style)
+  (declare (ignore text-style))
+  11)
+
+(defmethod clim3-port:text-ascent ((port test-port-2) text-style string)
+  (declare (ignore text-style string))
+  16)
+
+(defmethod clim3-port:text-descent ((port test-port-2) text-style string)
+  (declare (ignore text-style string))
+  6)
+
+(defmethod clim3-port:text-width ((port test-port-2) text-style string)
+  (declare (ignore text-style))
+  (* 9 (length string)))
+
 (defun test1 ()
-  (let ((port (make-instance 'test-port))
+  (let ((port1 (make-instance 'test-port-1))
+	(port2 (make-instance 'test-port-2))
 	(zone (clim3-text:text "hi" nil (clim3-color:make-color 1.0 0.0 0.0))))
-    (clim3-port:connect zone port)
+    (clim3-port:connect zone port1)
     (assert (not (clim3-zone:gives-valid-p zone)))
-    (assert (eq (clim3-zone:parent zone) port))
-    (assert (eq (clim3-zone:find-client zone) port))
-    (update port)
+    (assert (eq (clim3-zone:parent zone) port1))
+    (assert (eq (clim3-zone:find-client zone) port1))
+    (update port1)
     (assert (= (clim3-zone:width zone) 18))
     (assert (= (clim3-zone:height zone) 44))
-    (setf (clim3-text:chars zone) "bla")
+    (clim3-port:disconnect zone port1)
     (assert (not (clim3-zone:gives-valid-p zone)))
-    (update port)
+    (clim3-port:connect zone port2)
+    (update port2)
     (assert (clim3-zone:gives-valid-p zone))
-    (assert (= (clim3-zone:width zone) 26))
-    (assert (= (clim3-zone:height zone) 44))))
+    (assert (= (clim3-zone:width zone) 21))
+    (assert (= (clim3-zone:height zone) 37))))
 
 (defun test ()
   (test1))
