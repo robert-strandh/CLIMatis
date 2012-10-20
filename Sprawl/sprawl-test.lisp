c8dbfacb372663db2a9e230f0859c4f80d2d762d
Robert Strandh <strandh@nocl.(none)>
2012-10-20 08:34:08 +0200

    more tests

--- Sprawl/sprawl-test.lisp
+++ Sprawl/sprawl-test.lisp
@@ -3,8 +3,7 @@
 (defun hugep (sprawl)
   (null (clim3-sprawl:max-size sprawl)))
 
-(defun test-combine-in-parallel ()
-  ;; Test normal case.
+(defun test-combine-in-parallel-normal-case ()
   (loop repeat 1000000
 	do (let* ((length (1+ (random 20)))
 		  (size (random 20))
@@ -26,5 +25,46 @@
 		   (assert (<= (clim3-sprawl:size combined)
 			       (clim3-sprawl:max-size combined))))))))
 
+(defun test-combine-in-parallel-all-huge ()
+  (loop repeat 1000000
+	do (let* ((length (1+ (random 20)))
+		  (sprawls (loop repeat length
+				 collect (let ((min-size (random 20)))
+					   (clim3-sprawl:sprawl 
+					    min-size
+					    (+ min-size (random 5))
+					    nil))))
+		  (combined (clim3-sprawl:combine-in-parallel sprawls)))
+	     (assert (null (clim3-sprawl:max-size combined)))
+	     (assert (= (clim3-sprawl:min-size combined)
+			(reduce #'max sprawls :key #'clim3-sprawl:min-size))))))
+
+(defun test-combine-in-parallel ()
+  (test-combine-in-parallel-normal-case)
+  (test-combine-in-parallel-all-huge))
+
+(defun test-combine-in-series ()
+  (loop repeat 1000000
+	do (let* ((length (1+ (random 20)))
+		  (sprawls (loop repeat length
+				 collect (let* ((min-size (random 10))
+						(size (+ min-size (random 5)))
+						(max-size (if (zerop (random 2))
+							      nil
+							      (+ size (random 5)))))
+
+					   (clim3-sprawl:sprawl
+					    min-size size max-size))))
+		  (combined (clim3-sprawl:combine-in-series sprawls)))
+	     (assert (= (clim3-sprawl:min-size combined)
+			(reduce #'+ sprawls :key #'clim3-sprawl:min-size)))
+	     (assert (= (clim3-sprawl:size combined)
+			(reduce #'+ sprawls :key #'clim3-sprawl:size)))
+	     (if (some #'hugep sprawls)
+		 (assert (hugep combined))
+		 (assert (= (clim3-sprawl:max-size combined)
+			(reduce #'+ sprawls :key #'clim3-sprawl:max-size)))))))
+
 (defun test()
-  (test-combine-in-parallel))
+  (test-combine-in-parallel)
+  (test-combine-in-series))
