3eec14542c4a84123e0555f43ffdc62fd9771a9f
Robert Strandh <strandh@nocl.(none)>
2012-10-21 20:14:08 +0200

    fixed a problem

--- Test/slider.lisp
+++ Test/slider.lisp
@@ -1,43 +1,47 @@
+(defun brick (w h contents)
+  (clim3-layout:hframe*
+   w w w
+   (clim3-layout:vframe*
+    h h h
+    contents)))
+
 (defun slider ()
-  (let* ((background-color (clim3-color:make-color 0.0 1.0 0.0))
+  (let* ((slider-height 200)
+	 (slider-width 50)
+	 (button-height 30)
+	 (max-pos (- slider-height button-height))
+	 (background-color (clim3-color:make-color 0.0 1.0 0.0))
 	 (background (clim3-graphics:opaque background-color))
 	 (button-color (clim3-color:make-color 1.0 0.0 0.0))
 	 (button-background (clim3-graphics:opaque button-color))
 	 (pile nil)
 	 (pointer-vpos 0)
-	 (button-vpos nil)
+	 (pos-diff nil)
 	 (motion-handler (lambda (zone hpos vpos)
 			   (declare (ignore zone hpos))
-			   (format t "motion ~s~%" vpos)
-			   (if (null button-vpos)
+			   (if (null pos-diff)
 			       (setf pointer-vpos vpos)
 			       (setf (clim3-zone:vpos pile)
-				     (max 10 (min 90 (- vpos button-vpos)))))))
+				     (max 00 (min max-pos (+ vpos pos-diff)))))))
 	 (press-handler (lambda (zone code modifiers)
 			  (declare (ignore zone code modifiers))
-			  (format t "press~%")
-			  (setf button-vpos pointer-vpos)))
+			  (setf pos-diff (- (clim3-zone:vpos pile) pointer-vpos))))
 	 (release-handler (lambda (zone code modifiers)
 			    (declare (ignore zone code modifiers))
-			    (format t "release~%")
-			    (setf button-vpos nil)))
+			    (setf pos-diff nil)))
 	 (button-button (clim3-input:button press-handler release-handler)))
     (setf (clim3-zone:depth button-background) 10)
     (setf pile
-	  (clim3-layout:hframe*
-	   40 40 40
-	   (clim3-layout:vframe*
-	    20 20 20
-	    (clim3-layout:pile* button-button button-background))))
+	  (brick
+	   slider-width button-height
+	   (clim3-layout:pile* button-button button-background)))
     (setf (clim3-zone:hpos pile) 0)
-    (setf (clim3-zone:vpos pile) 80)
-    (clim3-layout:hframe*
-     40 40 40
-     (clim3-layout:vframe*
-      100 100 100
-      (clim3-layout:pile*
-       (clim3-input:motion motion-handler)
-       (clim3-layout:bboard* pile)
-       background)))))
+    (setf (clim3-zone:vpos pile) max-pos)
+    (brick
+     slider-width slider-height
+     (clim3-layout:pile*
+      (clim3-input:motion motion-handler)
+      (clim3-layout:bboard* pile)
+      background))))
 	
        
