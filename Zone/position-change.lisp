3fb9887287be949404094eacb4fd89d83142811c
Robert Strandh <strandh@nocl.(none)>
2012-10-24 17:19:42 +0200

    better comments, and added notify-child-depth-changed

--- Zone/position-change.lisp
+++ Zone/position-change.lisp
@@ -4,17 +4,34 @@
 ;;;
 ;;; Generic function NOTIFY-CHILD-POSITION-CHANGED.
 ;;;
-;;; This generic function is called whenever the position or size of
-;;; the zone is explicitly changed by a call to (SETF HPOS), (SETF
-;;; VPOS), (SETF WIDTH), or (SETF HEIGHT).
+;;; This generic function is called whenever the position of the zone
+;;; is explicitly changed by a call to (SETF HPOS) or (SETF VPOS).
 ;;;
-;;; Two default methods are supplied.  The first default method is
-;;; specialized on a NULL parent and it does nothing.  This way, a
-;;; zone and its parent are always valid arguments to this function,
-;;; even though the zone is disconnected.  The second default method
-;;; is specialized on a parent of type ZONE and it signals an error.
-;;; By doing it this way, we compel zones to make an explicit choice
-;;; concerning the action of this function.
+;;; A defult method is supplied that is specialized for a NULL parent,
+;;; and it does nothing.  That way, it is possible to set the position
+;;; of the root of a disconnected zone hierarchy.  
+;;;
+;;; When the zone hierarchy is connected to a port, the root zone of
+;;; the hierarchy has the port as its parent.  Therefore, ports should
+;;; supply a method for this function, specialized on that type of
+;;; port.  Some ports may decide to ignore the notification.  Others
+;;; may attempt to move the root window on the display. 
+;;;
+;;; When the parent is a layout zone, some appropriate action is
+;;; required.  Layout zones that determine the position of the
+;;; children themselves, such as the vbox and hbox zones provide a
+;;; method on this function that signals an error.  Layout zones that
+;;; allow their children to determine their own position, but that
+;;; need to be informed about when that happens, such as the bboard
+;;; zone, supply a method on this function that marks the layout zone
+;;; as having children with invalid positions. 
 
 (defgeneric notify-child-position-changed (child parent))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Generic function NOTIFY-CHILD-DEPTH-CHANGED.
+;;;
+;;; FIXME: say more
+
+(defgeneric notify-child-depth-changed (child parent))
