f96159541f2208249b03b1228ff7ac9159494a7a
Robert Strandh <strandh@nocl.(none)>
2012-11-05 13:26:04 +0100

    removed old paint function and moved new ones to separate file

--- Backends/CLX-Framebuffer/paint.lisp
+++ Backends/CLX-Framebuffer/paint.lisp
@@ -23,16 +23,61 @@
 			 (ash (round (* 255 new-b)) 0))))
       (setf (aref pa vp hp) new-pixel))))
 
-;;; Paint the part of the mask that falls within
-;;; the clipping region.
+(defmethod clim3-port:new-port-paint-opaque
+    ((port clx-framebuffer-port) color)
+  (let ((pa *pixel-array*)
+	(pixel (+ (ash (round (* 255 (clim3-color:red color))) 16)
+		  (ash (round (* 255 (clim3-color:green color))) 8)
+		  (ash (round (* 255 (clim3-color:blue color))) 0))))
+    (loop for vpos from *vstart* below *vend*
+	  do (loop for hpos from *hstart* below *hend*
+		   do (setf (aref pa vpos hpos) pixel)))))
+
 (defmethod clim3-port:new-port-paint-mask
     ((port clx-framebuffer-port) mask color)
-  (loop for hpos from hstart below hend
-	do (loop for vpos from vstart below vend
-		 do (clim3-port:paint-pixel
-		     *port* hpos vpos
-		     (clim3-color:red color)
-		     (clim3-color:green color)
-		     (clim3-color:blue color)
-		     (aref mask vpos hpos)))))
+  (let ((width (array-dimension mask 1))
+	(height (array-dimension mask 0))
+	(r (clim3-color:red color))
+	(g (clim3-color:green color))
+	(b (clim3-color:blue color)))
+    (loop for vpos from *vstart* below *vend*
+	  for row from (- *vstart* *vpos*) below height
+	  do (loop for hpos from *hstart* below *hend*
+		   for col from (- *hstart* *hpos*) below width
+		   do (let* ((pa *pixel-array*)
+			     (pixel (aref pa vpos hpos))
+			     (alpha (aref mask row col))
+			     (old-r (/ (logand 255 (ash pixel -16)) 255d0))
+			     (old-g (/ (logand 255 (ash pixel -8)) 255d0))
+			     (old-b (/ (logand 255 pixel) 255d0))
+			     (new-r (+ (* r alpha) (* old-r (- 1d0 alpha))))
+			     (new-g (+ (* g alpha) (* old-g (- 1d0 alpha))))
+			     (new-b (+ (* b alpha) (* old-b (- 1d0 alpha))))
+			     (new-pixel (+ (ash (round (* 255 new-r)) 16)
+					   (ash (round (* 255 new-g)) 8)
+					   (ash (round (* 255 new-b)) 0))))
+			(setf (aref pa vpos hpos) new-pixel))))))
 
+(defmethod clim3-port:new-port-paint-text
+    ((port clx-framebuffer-port) text text-style color)
+  (let* ((font (font port))
+	 (ascent (clim3-port:text-style-ascent port text-style)))
+    (unless (zerop (length text))
+      (flet ((y-pos (char)
+	       ;; I don't know why the -1 is necessary
+	       (+ -1 ascent (camfer:y-offset (camfer:find-glyph font char))))
+	     (mask (char)
+	       (camfer:mask (camfer:find-glyph font char))))
+	;; paint the first character
+	(clim3-port:with-position (0 (y-pos (char text 0)))
+	  (clim3-port:new-port-paint-mask port (mask (char text 0)) color))
+ 	(loop with pos-x = 0
+ 	      for i from 1 below (length text)
+ 	      for glyph = (camfer:find-glyph font (char text i))
+ 	      do (progn
+ 		   ;; compute the new x position
+ 		   (incf pos-x
+ 			 (+ (glyph-width font (char text (1- i)))
+ 			    (glyph-space font (char text (1- i)) (char text i))))
+		   (clim3-port:with-position (pos-x (y-pos (char text i)))
+		     (clim3-port:new-port-paint-mask port (mask (char text i)) color))))))))
