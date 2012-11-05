ebbc6d832d7483e818d6117938dc561a47c9a658
Robert Strandh <strandh@nocl.(none)>
2012-11-05 12:40:41 +0100

    make sure painting is bottom up, and add painting of input zones

--- Paint/paint.lisp
+++ Paint/paint.lisp
@@ -2,10 +2,14 @@
 
 (defgeneric new-paint (zone))
 
+(defmethod new-paint ((zone clim3-input:input))
+  nil)
+
 (defmethod new-paint ((zone clim3-zone:compound-mixin))
   (clim3-zone:impose-child-layouts zone)
-  (clim3-zone:map-over-children
+  (clim3-zone:map-over-children-bottom-to-top
    (lambda (child)
      (clim3-port:with-zone child
        (new-paint child)))
    zone))
+
