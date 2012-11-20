c32d7a84accc4ebc82896ce67066c2d800267c6b
Robert Strandh <strandh@picl.(none)>
2012-11-20 05:31:26 +0100

    instantiating glyphs

--- Fonts/TrueType/truetype.lisp
+++ Fonts/TrueType/truetype.lisp
@@ -20,6 +20,29 @@
   (format stream "[spline-segment start: ~s :control ~s]"
 	  (start object) (control object)))
 
+(defgeneric flip-segment (segment))
+
+(defmethod flip-segment ((segment line-segment))
+  (destructuring-bind (x . y) (start segment)
+    (make-instance
+     'line-segment
+     :start (cons x (- y)))))
+
+(defmethod flip-segment ((segment spline-segment))
+  (destructuring-bind (xs . ys) (start segment)
+    (destructuring-bind (xc . yc) (control segment)
+      (make-instance
+       'spline-segment
+       :start (cons xs (- ys))
+       :control (cons xc (- yc))))))
+
+(defun flip-contour (contour)
+  (let ((result (loop repeat (element-count contour)
+		      for segment in contour
+		      collect (flip-segment segment))))
+    (setf (cdr (last result)) result)
+    result))
+
 (defgeneric scale-segment (segment factor))
 
 (defmethod scale-segment ((segment line-segment) factor)
@@ -64,7 +87,6 @@
   (loop for contour across ttf-contours
 	collect (create-contour contour)))
 
-	  
 (defun scale-contour (contour factor)
   (let ((result (loop repeat (element-count contour)
 		      for segment in contour
@@ -104,3 +126,64 @@
 						(control segment)
 						(start next))))))
     result))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Creating instances of a TrueType font at a particular size.
+
+(defparameter +points-per-inch+ 72)
+
+;;; The concept of screen resolution is a tricky one.
+;;; Here, we just estimate it in terms of the number of
+;;; pixels per inch in a typical monitor. 
+(defparameter *screen-resolution* 100)
+
+(defclass glyph-instance ()
+  (;; The distance from the reference point to the left edge of the
+   ;; mask.  When positive, the left edge of the mask is to the right
+   ;; of the reference point. 
+   (%x-offset :initarg :x-offset :reader x-offset)
+   ;; The distance from the reference point to the top edge of the
+   ;; mask.  When positive, the top edge of the mask is below the
+   ;; reference point (which almost never happens).
+   (%y-offset :initarg :y-offset :reader y-offset)
+   ;; The amount to be added to the x value of the reference point to
+   ;; get the reference point of the glyph to follow.
+   (%advance-width :initarg :advance-width :reader advance-width)
+   ;; A 2-dimensional array of double floats, representing opacity
+   ;; values between 0 and 1.
+   (%mask :initarg :mask :reader mask)))
+
+(defun instantiate-glyph (units/em glyph point-size resolution)
+  (let* ((scale-factor (coerce (/ (* point-size resolution 1/72) units/em) 'double-float))
+	 (contours (create-contours (zpb-ttf:contours glyph)))
+	 (flipped-contours (mapcar #'flip-contour contours))
+	 (scaled-contours (loop for contour in flipped-contours
+				collect (scale-contour contour scale-factor)))
+	 (polygons (mapcar #'interpolate-contour scaled-contours))
+	 (advance-width (ceiling (* scale-factor (zpb-ttf:advance-width glyph)))))
+    (multiple-value-bind (mask min-x min-y)
+	(clim3-rendering:render-polygons polygons)
+      (make-instance 'glyph-instance
+		     :x-offset min-x
+		     :y-offset min-y
+		     :advance-width advance-width
+		     :mask mask))))
+
+(defclass font-instance ()
+  ((%font-loader :initarg :font-loader :reader font-loader)
+   (%size :initarg :size :reader size)
+   ;; A hash table in which the keys are the glyphs of the
+   ;; TrueType font, and the values are the glyph instances
+   (%glyph-instances :initform (make-hash-table :test #'eq)
+		     :reader glyph-instances)))
+
+(defun find-glyph-instance (font-instance character)
+  (let* ((font-loader (font-loader font-instance))
+	 (glyph (zpb-ttf:find-glyph character font-loader)))
+    (or (gethash glyph (glyph-instances font-instance))
+	(setf (gethash glyph (glyph-instances font-instance))
+	      (instantiate-glyph (zpb-ttf:units/em font-loader)
+				 glyph
+				 (size font-instance)
+				 *screen-resolution*)))))
