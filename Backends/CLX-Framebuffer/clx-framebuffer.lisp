ae9c0d92e3b8de9b4700b7da86c315af81b1a90e
Robert Strandh <strandh@nocl.(none)>
2012-11-22 17:23:57 +0100

    Simple text-style to font instance mapping

--- Backends/CLX-Framebuffer/clx-framebuffer.lisp
+++ Backends/CLX-Framebuffer/clx-framebuffer.lisp
@@ -368,6 +368,76 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
+;;; Mapping from text styles to font instances.
+;;;
+;;; Do this better later.
+
+(defparameter *camfer-sans-roman* (make-hash-table))
+
+(defparameter *freefont-directory* "/usr/share/fonts/truetype/freefont/")
+
+(defparameter *freefont-files*
+  '(((:fixed :roman)            "FreeMono.ttf")
+    ((:fixed :bold)             "FreeMonoBold.ttf")
+    ((:fixed :oblique)          "FreeMonoOblique.ttf")
+    ((:fixed (:bold :oblique))  "FreeMonoBoldOblique.ttf")
+    ((:sans :roman)             "FreeSans.ttf")
+    ((:sans :bold)              "FreeSansBold.ttf")
+    ((:sans :oblique)           "FreeSansOblique.ttf")
+    ((:sans (:bold :oblique))   "FreeSansBoldOblique.ttf")
+    ((:serif :roman)            "FreeSerif.ttf")
+    ((:serif :bold)             "FreeSerifBold.ttf")
+    ((:serif :italic)           "FreeSerifItalic.ttf")
+    ((:serif (:bold :italic))   "FreeSerifBoldItalic.ttf")))
+
+;;; Keys are lists of the form (<family> <face>).  Values are of the
+;;; form (<font loader> . <instances>) where <instances> is a hash
+;;; table with the size as a key and a font instance as the value.
+(defparameter *freefont-fonts* (make-hash-table :test #'equal))
+  
+(defun text-style-to-font-instance (text-style)
+  (with-accessors ((foundry clim3-text-style:foundry)
+		   (family clim3-text-style:family)
+		   (face clim3-text-style:face)
+		   (size clim3-text-style:size))
+      text-style
+    (ecase foundry
+      (:camfer (progn (unless (and (eq family :sans)
+				   (eq face :roman))
+			(error "Camfer font currently has only sans roman"))
+		      (or (gethash size *camfer-sans-roman*)
+			  (setf (gethash size *camfer-sans-roman*)
+				(camfer:make-font size 100)))))
+      (:free (let* ((family-face (list (clim3-text-style:family text-style)
+				       (clim3-text-style:face text-style)))
+		    (font (gethash family-face *freefont-fonts*)))
+	       (cond ((null font)
+		      (let ((filename (cadr (find family-face *freefont-files*
+						  :key #'car
+						  :test #'equal))))
+			(when (null filename)
+			  (error "No font file found"))
+			(let* ((pathname (concatenate 'string
+						      *freefont-directory*
+						      filename))
+			       (font-loader (zpb-ttf:open-font-loader pathname))
+			       (instances (make-hash-table)))
+			  (setf (gethash family-face *freefont-fonts*)
+				(cons font-loader instances))
+			  (let ((instance (clim3-truetype:instantiate-font
+					   font-loader
+					   size)))
+			    (setf (gethash size instances) instance)))))
+		    ((null (gethash size (cdr font)))
+		     (let ((instance (clim3-truetype:instantiate-font
+				      (car font)
+				      size)))
+		       (setf (gethash size (cdr font)) instance)))
+		    (t
+		     (gethash size (cdr font)))))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
 ;;; Keycodes, keysyms, etc.
 
 (defparameter *modifier-names*
