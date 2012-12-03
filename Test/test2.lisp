177db696eb18af77cd7ac18b5ce3bd88085b0327
Robert Strandh <strandh@nocl.(none)>
2012-12-03 11:48:52 +0100

    adapt test to new conventions

--- Test/test2.lisp
+++ Test/test2.lisp
@@ -2,20 +2,10 @@
 
 (defparameter *z2*
   (clim3-layout:hbox*
-   (clim3-layout:brick*
+   (clim3-layout:brick
     200 200
     (clim3-layout:pile*
      (clim3-graphics:opaque (clim3-color:make-color 1.0 0.0 0.0))
-     (clim3-input:key-press
-      (lambda (zone code modifiers)
-	(declare (ignore zone))
-	(format *debug-io* "red: key press: ~a ~a~%" code modifiers)
-	(force-output *debug-io*)))
-     (clim3-input:key-release
-      (lambda (zone code modifiers)
-	(declare (ignore zone))
-	(format *debug-io* "red: key release: ~a ~a~%" code modifiers)
-	(force-output *debug-io*)))
      (clim3-input:button
       (lambda (zone code modifiers)
 	(declare (ignore zone))
@@ -33,26 +23,19 @@
       (lambda (zone)
 	(declare (ignore zone))
 	(format *debug-io* "red: leave~%")
-	(force-output *debug-io*)))
+	(force-output *debug-io*))
+      (lambda (hpos vpos)
+	(< (+ (expt (- hpos 100) 2) (expt (- vpos 100) 2))
+	   (expt 50 2))))
      (clim3-input:motion
       (lambda (zone hpos vpos)
 	(declare (ignore zone))
 	(format *debug-io* "red: motion ~a ~a~%" hpos vpos)
 	(force-output *debug-io*)))))
-   (clim3-layout:brick*
+   (clim3-layout:brick
     200 200
     (clim3-layout:pile*
      (clim3-graphics:opaque (clim3-color:make-color 0.0 1.0 0.0))
-     (clim3-input:key-press
-      (lambda (zone code modifiers)
-	(declare (ignore zone))
-	(format *debug-io* "green: key press: ~a ~a~%" code modifiers)
-	(force-output *debug-io*)))
-     (clim3-input:key-release
-      (lambda (zone code modifiers)
-	(declare (ignore zone))
-	(format *debug-io* "green: key release: ~a ~a~%" code modifiers)
-	(force-output *debug-io*)))
      (clim3-input:button
       (lambda (zone code modifiers)
 	(declare (ignore zone))
