ba469da24fb5b3ae3d31f35ed9606b2caec12230
Robert Strandh <strandh@nocl.(none)>
2012-12-15 05:30:13 +0100

    embryonic button handlers

--- Applications/Calendar/calendar.lisp
+++ Applications/Calendar/calendar.lisp
@@ -107,7 +107,33 @@
      (clim3-layout:hbrick 10)))
    (clim3-graphics:opaque *background*)))
 
-(defun butcon (label)
+(defclass next-week-button-handler (clim3-port:button-handler)
+  ())
+
+(defmethod clim3-port:handle-button-press
+    ((handler next-week-button-handler) button-code modifiers)
+  (declare (ignore button-code modifiers))
+  (format t "next week press~%"))
+
+(defmethod clim3-port:handle-button-release
+    ((handler next-week-button-handler) button-code modifiers)
+  (declare (ignore button-code modifiers))
+  (format t "next week release~%"))
+
+(defclass previous-week-button-handler (clim3-port:button-handler)
+  ())
+
+(defmethod clim3-port:handle-button-press
+    ((handler previous-week-button-handler) button-code modifiers)
+  (declare (ignore button-code modifiers))
+  (format t "previous week press~%"))
+
+(defmethod clim3-port:handle-button-release
+    ((handler previous-week-button-handler) button-code modifiers)
+  (declare (ignore button-code modifiers))
+  (format t "previous release~%"))
+
+(defun butcon (label handler)
   (let* ((normal (clim3-graphics:opaque *background*))
 	 (darker (clim3-graphics:opaque (clim3-color:make-color 0.8d0 0.8d0 0.8d0)))
 	 (wrap (clim3-layout:wrap normal)))
@@ -115,9 +141,11 @@
      (clim3-input:visit
       (lambda (zone)
 	(declare (ignore zone))
+	(setf clim3-port:*button-handler* handler)
 	(setf (clim3-zone:children wrap) darker))
       (lambda (zone)
 	(declare (ignore zone))
+	(setf clim3-port:*button-handler* clim3-port:*null-button-handler*)
 	(setf (clim3-zone:children wrap) normal)))
      (clim3-text:text label *toolbar-text-style* *black*)
      wrap)))
@@ -126,9 +154,9 @@
   (clim3-layout:pile*
    (clim3-layout:hbox*
     (clim3-layout:sponge)
-    (butcon "<")
+    (butcon "<" (make-instance 'previous-week-button-handler))
     (clim3-layout:hbrick 20)
-    (butcon ">")
+    (butcon ">"(make-instance 'next-week-button-handler))
     (clim3-layout:sponge))
    (clim3-graphics:opaque *background*)))
 
