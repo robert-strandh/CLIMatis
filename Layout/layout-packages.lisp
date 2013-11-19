aeee7a6df89cdabc92eb1eabe10967636275ae3f
Robert Strandh <robert.strandh@gmail.com>
2013-11-19 08:33:00 +0100

    Removed symbols that are no longer used.

--- Layout/layout-packages.lisp
+++ Layout/layout-packages.lisp
@@ -1,13 +1,4 @@
 (defpackage #:clim3-layout
   (:use #:common-lisp #:clim3-zone)
   (:export
-   #:hbox #:hbox* #:vbox #:vbox*
-   #:pile #:pile*
-   #:grid
-   #:scroll #:scroll* #:hscroll #:hscroll* #:vscroll #:vscroll*
-   #:wrap #:wrap*
-   #:bboard #:bboard*
-   #:sponge #:sponge* #:hsponge #:hsponge* #:vsponge  #:vsponge*
-   #:brick #:brick* #:hbrick #:hbrick* #:vbrick #:vbrick*
-   #:hframe #:hframe* #:vframe #:vframe*
    ))
