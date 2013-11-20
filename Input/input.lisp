7874d1cca607488218ca6e0893b97a9e7a129e28
Robert Strandh <robert.strandh@gmail.com>
2013-11-20 11:35:57 +0100

    New class CLICKABLE and VISIT is no longer a zone.

--- Input/input.lisp
+++ Input/input.lisp
@@ -17,9 +17,14 @@
 ;;;
 ;;; Class VISIT
 ;;;
-;;; The handlers are called with the following arguments:
+;;; This class is not a subclass of zone.  Rather, it should be used
+;;; as a mixin superclass of a custom zone that should detect when the
+;;; pointer enters or leaves the zone.
 ;;;
-;;;   the zone
+;;; When the pointer enters the zone, the generic function ENTER is
+;;; called with the zone as an argument.  When the pointer leaves the
+;;; zone, the generic function LEAVE is called with the zone as an
+;;; argument.
 
 (defgeneric clim3:enter (zone)
   (:method-combination progn))
@@ -37,6 +42,53 @@
 
 (defclass clim3:visit ()
   ((%inside-p :initarg :inside-p :reader clim3:inside-p)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Class CLICKABLE.
+
+(defgeneric clim3:button-press (zone button)
+  (:method-combination progn))
+
+(defmethod clim3:button-press progn (zone button)
+  (declare (ignore zone button))
+  nil)
+
+(defgeneric clim3:button-release (zone button)
+  (:method-combination progn))
+
+(defmethod clim3:button-release progn (zone button)
+  (declare (ignore zone button))
+  nil)
+
+(defclass clim3:zone-button-handler (clim3:button-handler)
+  ((%zone :initarg :zone :reader zone)))
+
+(defmethod clim3:handle-button-press
+    ((button-handler clim3:zone-button-handler) code modifiers)
+  (clim3:button-press (zone button-handler)
+		      (clim3:standard-button-processor code modifiers)))
+
+(defmethod clim3:handle-button-release
+    ((button-handler clim3:zone-button-handler) code modifiers)
+  (clim3:button-release (zone button-handler)
+			(clim3:standard-button-processor code modifiers)))
+
+(defclass clim3:clickable ()
+  ((%previous :initform nil :accessor previous)))
+
+(defgeneric clim3:attention (zone))
+
+(defmethod clim3:attention ((zone clim3:clickable))
+  (setf (previous zone) clim3:*button-handler*)
+  (setf clim3:*button-handler*
+	(make-instance 'clim3:zone-button-handler :zone zone)))
+
+(defgeneric clim3:at-ease (zone))
+
+(defmethod clim3:at-ease ((zone clim3:clickable))
+  (setf clim3:*button-handler*
+	(previous zone)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
