36bab9499b12fbcf01e092a5444c98b34554cb64
Robert Strandh <robert.strandh@gmail.com>
2013-12-09 17:28:36 +0100

    Modified some defaults.

--- Gadgets/Default/theme.lisp
+++ Gadgets/Default/theme.lisp
@@ -4,13 +4,13 @@
   ()
   (:default-initargs
    :background-color (clim3:make-color 0.7 0.7 0.7)
-   :foreground-color (clim3:make-color 0.8 0.8 0.8)
-   :highlight-opacity 0.5
+   :foreground-color (clim3:make-color 0.8 0.8 0.9)
+   :highlight-opacity 0.3
    :highlight-color (clim3:make-color 1.0 1.0 1.0)
    :raise-border-thickness 3
-   :raise-border-opacity 0.5
+   :raise-border-opacity 0.3
    :sink-border-thickness 2
-   :sink-border-opacity 0.5
+   :sink-border-opacity 0.3
    :gadget-text-style (clim3:text-style :free :sans :roman 12)
    :gadget-text-color (clim3:make-color 0.0 0.0 0.0)
    :icon-border-thickness 5))
